fb452db23e9071b0618177387b1dc70a11d1a543
Lena Hierzi <lenahierzi@hotmail.com>
2023-10-14 23:00:12 +0100

    FE: add Material + tialwind unsuccessfully

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
 import celoGroups from "@celo/rainbowkit-celo/lists";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { ThemeProvider } from "@material-tailwind/react";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
@@ -41,11 +42,13 @@ const wagmiConfig = createConfig({
 function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RainbowKitProvider>
+      <ThemeProvider>
+        <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RainbowKitProvider>
+      </ThemeProvider>
     </WagmiConfig>
   );
 }
