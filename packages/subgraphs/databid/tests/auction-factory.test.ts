cdd56209dec6f121636b7cf953c58f2085cbd0f0
Nardos Tilahun <nardostilahun83@gmail.com>
2023-10-15 22:04:41 +0300

    Initialize subgraph

--- packages/subgraphs/databid/tests/auction-factory.test.ts
+++ packages/subgraphs/databid/tests/auction-factory.test.ts
@@ -7,10 +7,10 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { AuctionFinalized } from "../generated/schema"
-import { AuctionFinalized as AuctionFinalizedEvent } from "../generated/Auction/Auction"
-import { handleAuctionFinalized } from "../src/auction"
-import { createAuctionFinalizedEvent } from "./auction-utils"
+import { AuctionCreated } from "../generated/schema"
+import { AuctionCreated as AuctionCreatedEvent } from "../generated/AuctionFactory/AuctionFactory"
+import { handleAuctionCreated } from "../src/auction-factory"
+import { createAuctionCreatedEvent } from "./auction-factory-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
@@ -20,22 +20,20 @@ describe("Describe entity assertions", () => {
     let manager = Address.fromString(
       "0x0000000000000000000000000000000000000001"
     )
+    let newAuction = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
     let title = "Example string value"
     let maxOffer = BigInt.fromI32(234)
     let endDate = BigInt.fromI32(234)
-    let bidder = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let winningBid = BigInt.fromI32(234)
-    let newAuctionFinalizedEvent = createAuctionFinalizedEvent(
+    let newAuctionCreatedEvent = createAuctionCreatedEvent(
       manager,
+      newAuction,
       title,
       maxOffer,
-      endDate,
-      bidder,
-      winningBid
+      endDate
     )
-    handleAuctionFinalized(newAuctionFinalizedEvent)
+    handleAuctionCreated(newAuctionCreatedEvent)
   })
 
   afterAll(() => {
@@ -45,44 +43,38 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("AuctionFinalized created and stored", () => {
-    assert.entityCount("AuctionFinalized", 1)
+  test("AuctionCreated created and stored", () => {
+    assert.entityCount("AuctionCreated", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "AuctionFinalized",
+      "AuctionCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "manager",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "AuctionFinalized",
+      "AuctionCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "newAuction",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "AuctionCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "title",
       "Example string value"
     )
     assert.fieldEquals(
-      "AuctionFinalized",
+      "AuctionCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "maxOffer",
       "234"
     )
     assert.fieldEquals(
-      "AuctionFinalized",
+      "AuctionCreated",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "endDate",
-      "234"
-    )
-    assert.fieldEquals(
-      "AuctionFinalized",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "bidder",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "AuctionFinalized",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "winningBid",
       "234"
     )
 
