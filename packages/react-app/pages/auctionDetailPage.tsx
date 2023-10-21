59b7893f762918c4b5731a4d59829919366b158b
Lena Hierzi <lenahierzi@hotmail.com>
2023-10-21 03:59:25 +0100

    add set up to query subgraph ins FE

--- packages/react-app/pages/auctionDetailPage.tsx
+++ packages/react-app/pages/auctionDetailPage.tsx
@@ -1,9 +1,12 @@
 import AuctionDetail from "@/components/AuctionDetail";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import components from "../utils/components.json";
 import { auctions } from "../utils/mockData";
 import { Typography } from "@material-tailwind/react";
+import { graphClient } from "@/utils/graphClient";
+import { gql } from "@urql/core";
+import { allBidsRelatedToOneAuction, auctionById } from "@/utils/queries";
 
 const detailFields: Array<ComponentItem> = components.auctionItems;
 
@@ -11,11 +14,36 @@ export default function AuctionDetailPage() {
   const [userAddress, setUserAddress] = useState("");
   const { address, isConnected } = useAccount();
 
+  const auctionId = "";
+  const [project, setProject] = useState<AuctionItem>();
+  const [biddings, setBiddings] = useState<Array<BiddingItem>>();
+
+  const fetchAuctionData = useCallback(async () => {
+    const auctionResult = await graphClient
+      .query(gql(auctionById), { auctionId })
+      .toPromise();
+
+    console.log(auctionResult);
+    // auctionResult && setProject(auctionResult);
+
+    const biddingResult = await graphClient
+      .query(gql(allBidsRelatedToOneAuction), { auctionId })
+      .toPromise();
+
+    // biddingResult.length && setBiddings(biddingResult);
+
+    console.log(auctionResult);
+
+    // result.length && setProjects(result);
+  }, [auctionId]);
+
   useEffect(() => {
     if (isConnected && address) {
       setUserAddress(address);
     }
-  }, [address, isConnected]);
+
+    fetchAuctionData();
+  }, [address, isConnected, fetchAuctionData]);
 
   // TODO: get subgraph data on the auction
   // TODO: add dynamic routing
@@ -29,9 +57,6 @@ export default function AuctionDetailPage() {
           auctionItem={auctions[0]}
         ></AuctionDetail>
       </div>
-      {isConnected && (
-        <div className="h2 text-center">Your address: {userAddress}</div>
-      )}
     </div>
   );
 }
