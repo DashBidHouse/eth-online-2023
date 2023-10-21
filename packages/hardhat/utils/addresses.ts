7cb5c59293df5c9592f0fe3e9d5e4e2c61cb9f53
Lena Hierzi <lenahierzi@hotmail.com>
2023-10-21 01:48:52 +0100

    Add deployment script and address  file

---
+++ packages/hardhat/utils/addresses.ts
@@ -0,0 +1,14 @@
+export const addresses = {
+  optimismGoerli: {
+    usdc: '',
+    _optimisticOracleV3: '',
+  },
+  scroll: {
+    usdc: '',
+    _optimisticOracleV3: '',
+  },
+  mantle: {
+    usdc: '',
+    _optimisticOracleV3: '',
+  },
+};
